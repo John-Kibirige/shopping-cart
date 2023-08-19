96c67639e83f0a86b98e568a75d133b72e24799d
kibirige <kibirigejohn005@gmail.com>
2023-08-19 11:03:30 +0300

    implement the delete item when user clicks a given it

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import {
   ref,
   push,
   onValue,
+  remove,
 } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js';
 
 const appSettings = {
@@ -19,15 +20,50 @@ const inputFieldEl = document.getElementById('input-field');
 const addToCartButtonEl = document.getElementById('add-button');
 const shoppingListEl = document.getElementById('shopping-list');
 
+onValue(shoppingListInDB, (snapshot) => {
+  const data = snapshot.val();
+  if (snapshot.exists()) {
+    shoppingListEl.innerHTML = '';
+    for (const itemObject of Object.entries(data)) {
+      addShoppingItemToList(itemObject);
+    }
+  } else {
+    shoppingListEl.innerHTML =
+      '<li class="text-center bg-slate-200 w-full py-2 rounded">No items in the shopping list</li>';
+  }
+});
+
 addToCartButtonEl.addEventListener('click', () => {
   const inputValue = inputFieldEl.value;
+  if (inputValue === '') return;
   addShoppingItemToList(inputValue);
   push(shoppingListInDB, inputValue);
   resetInputField();
 });
 
-const addShoppingItemToList = (inputValue) => {
-  shoppingListEl.innerHTML += `<li>${inputValue}</li>`;
+const addShoppingItemToList = (itemObject) => {
+  const [id, item] = itemObject;
+  const shoppingItem = document.createElement('li');
+  shoppingItem.classList.add(
+    'bg-slate-300',
+    'px-6',
+    'py-2',
+    'rounded',
+    'cursor-pointer',
+    'hover:bg-slate-400',
+    'transition',
+    'duration-300',
+    'ease-in-out'
+  );
+
+  shoppingItem.textContent = item;
+  shoppingListEl.appendChild(shoppingItem);
+
+  shoppingItem.addEventListener('click', () => {
+    shoppingItem.remove();
+    const itemLocation = ref(database, `shopping-list/${id}`);
+    remove(itemLocation);
+  });
 };
 
 const resetInputField = () => (inputFieldEl.value = '');
